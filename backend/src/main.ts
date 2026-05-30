bd44f013c85124276dd711dd579c71faa91be694
PatcharaPN <patchara.pn31@gmail.com>
2026-05-30 19:58:22 +0700

    fix: fix wrong cors string

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://partify-b4hu.onrender.com',
+    origin: 'https://partify-nine.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(
